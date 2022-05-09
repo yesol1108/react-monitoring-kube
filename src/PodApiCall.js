9339f9485f37bdf5de2b31ff8f3d489e7d816429
DESKTOP-09J2L5V\yesol <yesol1108@naver.com>
2022-05-09 15:06:36 +0900

    lllll

--- src/PodApiCall.js
+++ src/PodApiCall.js
@@ -1,31 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function PodMonitoring() {
-  
+const apiurl = 'https://api.ocp49.sandbox1231.opentlc.com:6443';
+const apitoken = 'Bearer sha256~SwyOFZrGGhY8BYRuTUC8YAA2CSOcn0NZVyLTVaF7enQ';
+
+function PodApiCall() {
+
   const [pods, setPods] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [lastResourceVersion, setResourceVersion] = useState(null);
+  const [podId, setPodId] = useState(null);
   
   const fetchPods = async () => {
     try {
       setError(null);
       setPods(null);
+      setPodId(null),
       setLoading(true);
       setResourceVersion(null);
 
       const response = await axios.get(
         'https://api.ocp49.sandbox1231.opentlc.com:6443/api/v1/pods', {
           headers: {
-            "Authorization": 'Bearer sha256~OnY5typhcXCIXxxwV8CsYkxVnfCSIndA0lr-efYxRSc'
+            "Authorization": 'Bearer sha256~SwyOFZrGGhY8BYRuTUC8YAA2CSOcn0NZVyLTVaF7enQ'
           }
         }
       );
 
       setResourceVersion(response.data.metadata.resourceVersion);
       setPods(response.data.items);
-
+      pods.forEach((pod) => {
+        let podIdtmp = `${pod.metadata.namespace}-${pod.metadata.name}`
+        setPodId(podIdtmp);
+      });
     } catch (e) {
       setError(e);
     }
@@ -40,7 +48,9 @@ function PodMonitoring() {
           "Authorization": 'Bearer sha256~SwyOFZrGGhY8BYRuTUC8YAA2CSOcn0NZVyLTVaF7enQ'
         }
       }
-    );
+    ).then(response => {
+
+    });
   }
   
   
@@ -67,4 +77,4 @@ function PodMonitoring() {
 
 }
 
-export default PodMonitoring;  
+export default PodApiCall;  
