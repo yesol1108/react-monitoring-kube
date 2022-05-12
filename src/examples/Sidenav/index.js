9524e8b625a4cf680e7d2db4e36e925d932a2ea4
DESKTOP-09J2L5V\yesol <yesol1108@naver.com>
2022-05-12 17:41:23 +0900

    ocp 로고 크기 조정

--- src/examples/Sidenav/index.js
+++ src/examples/Sidenav/index.js
@@ -161,7 +161,7 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
           </MDTypography>
         </MDBox>
         <MDBox component={NavLink} to="/" display="flex" alignItems="center">
-          {brand && <MDBox component="img" src={brand} alt="Brand" width="2rem" />}
+          {brand && <MDBox component="img" src={brand} alt="Brand" />}
           <MDBox sx={(theme) => sidenavLogoLabel(theme, { miniSidenav })}>
             <MDTypography component="h6" variant="button" fontWeight="medium" color={textColor}>
               {brandName}
