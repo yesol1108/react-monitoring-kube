4d505ccd950fa669f966d7c2b8edfae1b24da492
DESKTOP-09J2L5V\yesol <yesol1108@naver.com>
2022-05-17 20:23:49 +0900

    no message

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
-// const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// module.exports = function (app) {
-//   app.use(
-//     "/api",
-//     createProxyMiddleware({
-//       target: "https://api.ocp49.sandbox1411.opentlc.com:6443",
-//       changeOrigin: true,
-//     })
-//   );
-// };
+module.exports = function (app) {
+  app.use(
+    "/post",
+    createProxyMiddleware({
+      target: "https://api.ocp49.sandbox1411.opentlc.com:6443",
+      changeOrigin: true,
+    })
+  );
+};
